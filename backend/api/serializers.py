c608d2e7db96f8bc2bce45984e74157851ab44d4
Taylor Wingard <twingard@raybeam.com>
2018-07-14 20:27:04 -0700

    working on serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 
 
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ('url', 'text', 'asker', 'uuid')
